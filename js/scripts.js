1b98fd51ee2537742e0c189940ad08d11a1cf7c1
Nadiya Yeroshkina <nadin.yeroshkina@email.com>
2016-03-25 10:15:41 -0700

    add vars to basic js/jq

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    event.preventDefault();
+
+    var active = $("#active").val();
+    var weather = $("#weather").val();
+    var adrenaline = $("#adrenaline").val();
+    var accompany = $("#accompany").val();
+    var cuntry = $("#cuntry").val();
+
+  }
+});
